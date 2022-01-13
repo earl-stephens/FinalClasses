26b3513b4c55eec464cc93df3bc9f073ad7c6953
earl-stephens <stephensearl@hotmail.com>
2022-01-12 22:59:18 -0700

    Show use of final keyword

--- src/app/App.java
+++ src/app/App.java
@@ -1,5 +1,17 @@
 package app;
 
+final class Person {
+	
+}
+/* this gives an error because Person is a final
+ * class and you can't extend it.
+ * Used in Java for classes like String
+ 
+class Employee extends Person {
+	
+}
+*/
+
 public class App {
 
 	public static void main(String[] args) {
